b8eb5debf3bf9d09d26d2c8dbefdef2b6b03fe75
alansgoncalves <vekio.soft@gmail.com>
2022-08-22 08:38:32 -0300

    feat: create Categories page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import "./App.css";
 import { Route } from "react-router-dom";
+import Categories from "./pages/Categories";
 import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div className="App">
       <Route path="/" component={HomePage} exact />
+      <Route path="/categories" component={Categories} exact />
     </div>
   );
 }
